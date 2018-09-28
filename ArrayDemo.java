f6d8281355eaadc38a481ad78cd9b408878cb257
shzwon <rwu5@stuy.edu>
2018-09-28 18:44:05 -0400

    array print out functions done

---
+++ ArrayDemo.java
@@ -0,0 +1,21 @@
+public static void printArray(int[]ary){
+  for(int i = 0; i < ary.length; i++){
+    system.out.println(ary[i]);
+  }
+}
+
+public static void printArray(int[][]ary){
+  for(int i = 0; i < ary.length; i++){
+    for(int j = 0; j < ary[i].length; j++){
+      system.out.println(ary[i][j])
+    }
+  }
+}
+
+
+
+
+
+public static void main(String[] args) {
+  System.out.println(
+}
