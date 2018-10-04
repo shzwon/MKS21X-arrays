d0fd3c949b2aa42031bbb951603e11f67d04cd22
shzwon <rwu5@stuy.edu>
2018-10-04 11:14:43 -0400

    commiting files forgot

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,21 +1,29 @@
-public static void printArray(int[]ary){
-  for(int i = 0; i < ary.length; i++){
-    system.out.println(ary[i]);
-  }
-}
-
-public static void printArray(int[][]ary){
-  for(int i = 0; i < ary.length; i++){
-    for(int j = 0; j < ary[i].length; j++){
-      system.out.println(ary[i][j])
+public class ArrayDemo{
+  public static void printArray(int[]ary){
+    for(int i = 0; i < ary.length; i++){
+      system.out.println(ary[i]);
     }
   }
+  public static void printArray(int[][]ary){
+    for(int i = 0; i < ary.length; i++){
+      for(int j = 0; j < ary[i].length; j++){
+        system.out.println(ary[i][j])
+      }
+    }
+  }
+
+
+  public static void fill2D(int[][] vals){
+    int lens = vals.length;
+    for (i = 0; i < lens; i++)
+      
+
 }
 
 
 
-
-
-public static void main(String[] args) {
+  public static void main(String[] args) {
   System.out.println(
+}
+
 }
