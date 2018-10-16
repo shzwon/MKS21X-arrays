d014e99b77eb2ffca8e0647b17a51b28a8881a92
shzwon <rwu5@stuy.edu>
2018-10-16 11:39:34 -0400

    commmit

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,29 +1,75 @@
-public class ArrayDemo{
-  public static void printArray(int[]ary){
-    for(int i = 0; i < ary.length; i++){
-      system.out.println(ary[i]);
+public class ArrayDemo {
+  public static void printArray(int[] array){
+    String string = "{";
+    for(int i = 0; i<array.length; i++){
+      string += array[i];
+      if (i< array.length-1){
+        string += ",";
+      }
     }
+    string += "}";
+    System.out.println(string);
   }
-  public static void printArray(int[][]ary){
-    for(int i = 0; i < ary.length; i++){
-      for(int j = 0; j < ary[i].length; j++){
-        system.out.println(ary[i][j])
+
+  public static void printArray(int[][] array){
+    String string = "{";
+    for(int i = 0; i<array.length; i++){
+      string += "{";
+      for(int j=0; r<array[i].length; j++){
+        string+=array[i][j];
+        if (r<array[i].length-1){
+          string += ",";
+        }
+      }
+    string += "},\n";
+  }
+    string += "}";
+    System.out.println(string);
+  }
+
+
+
+  public static int countZeros2D(int[][] vals){
+    int count = 0;
+    for (int i=0; i<vals.length; i++) {
+      for (int j=0; j<vals[i].length; j++) {
+				if(array[i][j] == 0) {
+					count += 1;
+				}
+			}
+		}
+		return count;
+	}
+
+  public static void fill2D(int[][] vals){
+    for (int i=0; i<vals.length; i++) {
+      for (int j=0; j<vals[i].length; j++) {
+        if (i==j) {
+          vals[i][j] = 3;
+        } else {
+          vals[i][j] = 1;
+        }
       }
     }
   }
 
+  public static int[][] fill2DCopy(int[][] vals){
+      int[][] ans = new int[vals.length][vals[0].length];
+      for (int i=0; i<vals.length; i++) {
+        for (int j=0; j<vals[i].length; j++) {
+          if (vals[i][j] < 0) {
+            ans[i][j] = 3;
+          } else {
+            ans[i][j] = 1;
+          }
+        }
+      }
+      return ans;
+    }
 
-  public static void fill2D(int[][] vals){
-    int lens = vals.length;
-    for (i = 0; i < lens; i++)
-      
-
-}
 
 
 
-  public static void main(String[] args) {
-  System.out.println(
-}
+
 
 }
